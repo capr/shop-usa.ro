478feb27f51f521a00292e8b3021146c7d190bc6
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-11-18 07:07:58 +0200

    unimportant

--- www/checkout.js
+++ www/checkout.js
@@ -25,7 +25,9 @@ function create_login_section(dst_id) {
 
 	$('.btn_facebook').click(function() {
 		facebook_login(function(auth) {
-			post('/login.json', auth)
+			post('/login.json', auth, function(usr) {
+				checkout_update_account()
+			})
 		}, function() {
 			alert('Failed')
 		})
@@ -34,7 +36,7 @@ function create_login_section(dst_id) {
 
 function checkout_update_account(usr) {
 
-	if (!usr.emailvalid && !usr.haspass)
+	if (!usr.emailvalid == 1, && !usr.haspass)
 		create_login_section('#account_section')
 	else
 		apply_template('#account_section_template', {}, '#account_section')
--- www/login.json.lua
+++ www/login.json.lua
@@ -2,4 +2,22 @@
 check(POST)
 local auth = json(POST.data)
 local uid = login(auth)
-pp(query1('select * from usr where uid = ?', uid))
+
+local t = query1([[
+	select
+		u.firstname,
+		u.lastname,
+		u.email,
+		u.emailvalid,
+		u.pass is not null as haspass
+	from
+		usr u
+	where
+		u.active = 1
+		and u.uid = ?
+]], uid)
+
+t.emailvalid = t.emailvalid == 1
+t.haspass = t.haspass == 1
+
+out_json(t)
