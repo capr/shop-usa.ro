240739cf8886b124e19c9e4085d798b7b7353482
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-11-23 03:26:51 +0200

    unimportant

--- www/google.js
+++ www/google.js
@@ -1,27 +1,29 @@
 
+(function()
+var params = {
+	clientid: C('google_client_id'),
+	scope:    'https://www.googleapis.com/auth/plus.login email',
+	requestvisibleactions: 'http://schema.org/AddAction',
+	cookiepolicy: 'single_host_origin',
+}
+params.callback = function(authResult) {
+	if (authResult.status.signed_in) {
+		post('/login.json', {
+			type: 'google',
+			access_token: authResult.access_token,
+		}, success, fail)
+	} else {
+		// Possible error values:
+		//   "user_signed_out" - User is signed-out
+		//   "access_denied" - User denied access to your app
+		//   "immediate_failed" - Could not automatically log in the user
+		if (fail) fail()
+	}
+}
 function google_login(success, fail) {
-	var params = {
-		clientid: C('google_client_id'),
-		scope:    'https://www.googleapis.com/auth/plus.login email',
-		requestvisibleactions: 'http://schema.org/AddAction',
-		cookiepolicy: 'single_host_origin',
-	}
-	params.callback = function(authResult) {
-		if (authResult.status.signed_in) {
-			post('/login.json', {
-				type: 'google',
-				access_token: authResult.access_token,
-			}, success, fail)
-		} else {
-			// Possible error values:
-			//   "user_signed_out" - User is signed-out
-			//   "access_denied" - User denied access to your app
-			//   "immediate_failed" - Could not automatically log in the user
-			if (fail) fail()
-		}
-	}
 	gapi.auth.signIn(params)
 }
+)()
 
 function google_logout() {
 	gapi.auth.signOut()
