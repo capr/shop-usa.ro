d815687988b07935537b48c500ad61f1abafbbf9
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-11-23 03:50:37 +0200

    unimportant

--- www/config.js
+++ www/config.js
@@ -2,3 +2,21 @@
 C('facebook_app_id', '725102964211663')
 C('google_client_id', '113821693132-an9cmghgm2fockigiubs1rp7tmfr9vnb.apps.googleusercontent.com')
 
+if (lang() == 'ro') {
+S('move_here', 'Pune-l aici')
+S('remove', 'Sterge')
+S('price', '{0} Lei')
+S('na', '<span class=notavailable>Nedisponibil</span>')
+S('not_available', 'Nedisponibil')
+S('out_of_stock', 'Lipsa stoc')
+S('plenty_in_stock', '<b>In stoc</b>')
+S('low_stock', '<b>Doar {0} bucati</b> in stoc')
+S('in_stock', '<b>{0} bucati</b> in stoc')
+S('not_available_msg', 'Combinatie nedisponibila.\nTe rugam alege alta combinatie.')
+S('login_failed', 'Nu am reusit sa te logam.')
+S('email_required_error', 'Adauga email-ul')
+S('email_format_error', 'Email-ul e ceva gen foo@bar.com')
+S('pass_required_error', 'Adauga o parola')
+S('pass_format_error', 'Pune macar {0} caractere')
+}
+
