f9a7d5f939150230d1b55d7d381702ac1296c3a7
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-11-27 00:34:19 +0200

    unimportant

--- www/reset_pass.json.lua
+++ www/reset_pass.json.lua
@@ -1,2 +1,3 @@
 local pass = assert(json(POST.data).pass)
 set_pass(pass)
+out(json{ok = true}) --print a dummy json so that the ajax call doesn't fail.
--- www/send_token.json.lua
+++ www/send_token.json.lua
@@ -1,3 +1,3 @@
 local email = assert(json(POST.data).email)
 send_auth_token(email)
-out(json{ok = true})
+out(json{ok = true}) --print a dummy json so that the ajax call doesn't fail.
