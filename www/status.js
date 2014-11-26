cc547182f73a898691da7ec42d9c4f1713dc7343
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-11-26 04:47:08 +0200

    unimportant

--- www/status.js
+++ www/status.js
@@ -4,7 +4,7 @@ var g_usr
 function init_status() {
 	get('/login.json', function(usr) {
 		g_usr = usr
-		$('#usr_name').html(usr.name)
+		$('#usr_name').html(firstname(usr.name))
 		setlink('#usr_name', '/browse/account')
 	})
 }
