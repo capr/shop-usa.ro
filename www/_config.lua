f93438c0d3b21230b3a409e593d46dc2901de7fe
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-12-07 06:20:54 +0200

    unimportant

--- www/_config.lua
+++ www/_config.lua
@@ -4,7 +4,7 @@ if config('lang', 'en') == 'ro' then
 
 S('reset_pass_subject', 'Linkul pentru schimbarea parolei')
 
-S('order_placed_subject', 'Comanda %s la %s')
+S('order_placed_subject', 'Comanda nr. %s la %s')
 S('shiptype_home', 'Prin curier')
 S('shiptype_store', 'La magazin')
 S('sales', 'comenzi')
