024ef32939e4638af9e332e4c4e2508ae6d0fbbb
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-12-17 19:55:19 +0200

    unimportant

--- www/grid.js
+++ www/grid.js
@@ -15,7 +15,7 @@ function grid(data, dst) {
 			var row = data.rows[i]
 			for (var j = 0; j < data.fields.length; j++) {
 				if (!row[j])
-					row[j] = 'null'
+					row[j] = '&nbsp;'
 			}
 		}
 	}
@@ -55,31 +55,42 @@ function grid(data, dst) {
 
 	var active_cell
 	var active_input
-	function select_cell(cell, caret) {
-		if (!cell.length) return
 
-		// remove the input on the active cell
-		if (active_cell) {
+	function caret() {
+		return active_input && active_input.caret()
+	}
+
+	function set_edit(cell, caret) {
+		if (active_input) {
 			active_cell.find('span').html(active_input.val())
 			active_cell.find('div').html('')
+			active_input = null
 		}
-
+		if (!cell) return
 		// compute the dimensions of the input box based on text width and cell height.
 		// the edit box outer width should not exceed text width, to avoid reflowing.
-		var span = cell.find('>span')
-		var val = span.html().trim()
+		var val = cell.find('>span').html().trim()
 		var w = cell.width()
 		var h = cell.height()-1
-
-		// create the input box and focus it
 		cell.find('div').html('<input type=text class=input style="width: '+w+'px; height: '+h+'px;" value="' + val + '">')
 		var input = cell.find('input')
 		input.focus()
-
-		// set globals
-		active_cell = cell
+		input.caret(caret)
 		active_input = input
-		active_input.caret(caret)
+	}
+
+	function select_cell(cell, caret) {
+		if (!cell.length) return
+
+		if (active_cell) {
+			active_cell.removeClass('selected')
+			set_edit()
+		}
+		cell.addClass('selected')
+		active_cell = cell
+
+		if (g.immediate_mode)
+			set_edit(cell, caret)
 	}
 	g.selected_col = function(i, caret) {
 		if (typeof i != 'number')
@@ -97,40 +108,42 @@ function grid(data, dst) {
 
 	$(document).keydown(function(e) {
 		var altshift = e.altKey && e.shiftKey
-		if (e.which == 39 && (altshift || (
+		if (e.which == 39 && ((!g.immediate_mode && !active_input) || (g.immediate_mode && (altshift || (
 					active_input.is(':focus') &&
-					active_input.caret() == active_input.val().length &&
-					!e.shiftKey)
-		)) {
+					caret() == active_input.val().length && !e.shiftKey))))
+		) {
 			// right
 			g.selected_col(g.selected_col() + 1, 0)
 			e.preventDefault()
-		} else if (e.which == 37 && (altshift || (
-					active_input.is(':focus') &&
-					active_input.caret() == 0 &&
-					!e.shiftKey)
-		)) {
+		} else if (e.which == 37 && ((!g.immediate_mode && !active_input) || (g.immediate_mode && (altshift || (
+					active_input.is(':focus') && caret() == 0 && !e.shiftKey))))
+		) {
 			// left
 			g.selected_col(g.selected_col() - 1, -1)
 			e.preventDefault()
 		} else if (e.which == 38) {
 			// up
-			g.selected_row(g.selected_row() - 1, active_input.caret())
+			g.selected_row(g.selected_row() - 1, caret())
 			e.preventDefault()
 		} else if (e.which == 40) {
 			// down
-			g.selected_row(g.selected_row() + 1, active_input.caret())
+			g.selected_row(g.selected_row() + 1, caret())
 			e.preventDefault()
 		} else if (e.which == 33) {
 			// page up
 			var row = Math.max(0, g.selected_row() - g.page_rows)
-			g.selected_row(row, active_input.caret())
+			g.selected_row(row, caret())
 		} else if (e.which == 34) {
 			// page down
 			var row = Math.min(g.row_count()-1, g.selected_row() + g.page_rows)
-			g.selected_row(row, active_input.caret())
-		} else {
-			console.log(e.which)
+			g.selected_row(row, caret())
+		} else if (e.which == 13) {
+			// enter
+			if (!g.immediate_mode)
+				set_edit(active_cell, 0)
+		} else if (e.which == 27) {
+			// esc
+			set_edit()
 		}
 	})
 
