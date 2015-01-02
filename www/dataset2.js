34172789f7eb496271ca69e36df2b7251f24dfb7
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2015-01-02 13:56:56 +0200

    unimportant

--- www/dataset2.js
+++ www/dataset2.js
@@ -4,6 +4,8 @@ var dataset, ajax_dataset
 
 // helpers -------------------------------------------------------------------
 
+var undefined
+
 function assert(t, err) {
 	if (t == null || t === false)
 		throw (err || 'assertion failed')
@@ -15,12 +17,7 @@ function clamp(x, x0, x1) {
 }
 
 function insert(a, i, e) {
-	if (i >= a.length) {
-		// make sparse array: splice won't insert here.
-		if (e !== undefined)
-			a[i] = e
-	} else
-		a.splice(i, 0, e)
+	a.splice(i, 0, e)
 }
 
 function remove(a, i) {
@@ -31,20 +28,33 @@ function remove(a, i) {
 
 function range(i0, i1) {
 	var a = []
-	for(var i = i0; i <= i1; i++)
+	var i = i0
+	for (var i = i0; i <= i1; i++)
 		a.push(i)
 	return a
 }
 
+function sorted_keys(o, cmp) {
+	var t = []
+	for (var k in o)
+		if (o.hasOwnProperty(k))
+			t.push(k)
+	t.sort(cmp)
+	return t
+}
+
 function make_url(path, opt_args, opt_params) {
 	var args = []
-	for (var i in opt_args)
+	for (var i = 0; i < opt_args.length; i++)
 		args.push(encodeURIComponent(opt_args[i]))
 	args = args.join('/')
 
 	var params = []
-	for (var k in opt_params)
+	var t = sorted_keys(opt_params)
+	for (var i = 0; i < t.length; i++) {
+		var k = t[i]
 		params.push(encodeURIComponent(k)+'='+encodeURIComponent(opt_params[k]))
+	}
 	params = params.join('&')
 
 	return path+(args?'/'+args:'')+(params?'?'+params:'')
@@ -78,9 +88,9 @@ dataset = function(d_opt) {
 	var filter      // function(values, ri) -> true|false
 
 	var fieldindex_byname = function(fieldname) {
-		for (var i in fields)
-			if (fields[i].name == fieldname)
-				return i
+		for (var fi = 0; fi < fields.length; fi++)
+			if (fields[fi].name == fieldname)
+				return fi
 	}
 
 	var init_locals = function() {
@@ -100,23 +110,23 @@ dataset = function(d_opt) {
 		rowmap = []
 		for (var ri = 0; ri < rows.length; ri++) {
 			var row = rows[ri]
+			var cn = row.childcount || 0
 
-			// skip if deleted
-			if (row.deleted)
+			// deleted? skip it and all the children
+			if (row.deleted) {
+				ri += cn
+				continue
+			}
+
+			// filtered? skip it
+			if (filter && !filter(row))
 				continue
 
-			// skip if filtered
-			if (filter)
-				if (!filter(row))
-					continue
+			rowmap.push(ri) // add it
 
-			// add the index to the rowmap
-			rowmap.push(ri)
-
-			// skip all children of the record if the record is collapsed
-			var n = row.childcount
-			if (n && !row.expanded)
-				ri += n
+			// collapsed? skip children
+			if (cn && !filter && !row.expanded)
+				ri += cn
 		}
 	}
 
@@ -150,8 +160,8 @@ dataset = function(d_opt) {
 		if (validate && !validate(v, field, row, fields))
 			return false
 
-		// save old values if not already saved
-		if (!row.oldvalues)
+		// save old values if not already saved and the row is not new
+		if (!row.isnew && !row.oldvalues)
 			row.oldvalues = row.values.slice(0)
 
 		// set the value
@@ -169,14 +179,11 @@ dataset = function(d_opt) {
 		var rec = []
 
 		// add server_default values or null
-		for (var fi in fields) {
+		for (var fi = 0; fi < fields.length; fi++) {
 			var field = fields[fi]
 			var val = field.server_default
-			rec.push(val !== undefined ? val : null)
+			rec.push(val != null ? val : null)
 		}
-
-		// set record id to a client-generated id
-		set_client_id(rec)
 
 		return {values: rec, isnew: true}
 	}
@@ -191,23 +198,11 @@ dataset = function(d_opt) {
 		if (ri === undefined)
 			ri = rows.length
 
+		// get parent row at index
+		var parent_row = rows[ri] && rows[ri].parent_row
+
 		// make a new row
 		var row = d.new_row()
-
-		// make it a sibling of the row it displaced
-		if (rows[ri]) {
-			var prow = rows[ri].parent_row
-			if (prow) {
-				// set parent->child and child->parent links
-				row.parent_row = prow
-				prow.children.push(row)
-				// increment childcount in all parents
-				while (prow) {
-					prow.childcount++
-					prow = prow.parent_row
-				}
-			}
-		}
 
 		// insert the new row at ri
 		insert(rows, ri, row)
@@ -215,14 +210,17 @@ dataset = function(d_opt) {
 		// update idmap on ri
 		update_idmap(ri)
 
+		// add the row to the tree
+		add_to_tree(row, parent_row)
+
 		// recreate rowmap
 		init_rowmap()
 
 		// set client defaults
-		for (var fi in fields) {
+		for (var fi = 0; fi < fields.length; fi++) {
 			var field = fields[fi]
 			var val = field.client_default
-			if (val !== undefined)
+			if (val != null)
 				d.setval(ri, fi, val)
 		}
 	}
@@ -235,13 +233,20 @@ dataset = function(d_opt) {
 		var ri = rowmap[vri]
 		var row = rows[ri]
 
-		if (row.isnew)
-			remove(rows, ri)
-		else
-			row.deleted = true
+		// mark row as deleted
+		row.deleted = true
+
+		// mark children as deleted too
+		delete_children(row)
 
 		// recreate rowmap
 		init_rowmap()
+	}
+
+	var remove_row = function() {
+		for (var ri = 0; ri < rows.length; ri++) {
+
+		}
 	}
 
 	d.init = function() {
@@ -253,16 +258,6 @@ dataset = function(d_opt) {
 	}
 
 	// row id and row index by id aspect --------------------------------------
-
-	// client row id aspect
-	d.last_id = 0
-	d.new_id = function() {
-		return --d.last_id // use negative values for client-generated ids
-	}
-
-	var set_client_id = function(rec) {
-		rec[id_fi] = d.new_id()
-	}
 
 	var update_idmap = function(ri) {
 		var id = rows[ri].values[id_fi]
@@ -281,7 +276,7 @@ dataset = function(d_opt) {
 
 		// init idmap based on id_fi
 		idmap = {}
-		for (var ri in rows)
+		for (var ri = 0; ri < rows.length; ri++)
 			update_idmap(ri)
 
 	}
@@ -295,6 +290,7 @@ dataset = function(d_opt) {
 	d.initially_expanded = false
 
 	var parent_fi // field index of the parent field
+	var root_rows
 
 	var init_tree = function() {
 
@@ -306,10 +302,12 @@ dataset = function(d_opt) {
 		if (!parent_fi) return
 
 		// make tree
-		var root_rows = []
-		for (var ri in rows) {
+		root_rows = []
+		d.root_rows = []
+		var expanded = d.initially_expanded
+		for (var ri = 0; ri < rows.length; ri++) {
 			var row = rows[ri]
-			row.expanded = d.initially_expanded
+			row.expanded = expanded
 			var pid = row.values[parent_fi]
 			if (pid != null) {
 				var prow = rows[idmap[pid]]
@@ -330,7 +328,7 @@ dataset = function(d_opt) {
 		rows = []
 		d.rows = rows
 		function push_rows(root_rows) {
-			for (ri in root_rows) {
+			for (var ri = 0; ri < root_rows.length; ri++) {
 				var row = root_rows[ri]
 				rows.push(row)
 				if (row.children)
@@ -338,6 +336,67 @@ dataset = function(d_opt) {
 			}
 		}
 		push_rows(root_rows)
+
+		// compute and store direct+indirect child counts
+		function count_children(row) {
+			if (!row.children)
+				return 0
+			var rows = row.children
+			var n = rows.length // direct child count
+			for (var i = 0; i < rows.length; i++)
+				n += count_children(rows[i])
+			row.childcount = n  // store count
+			return n
+		}
+		for (var i = 0; i < root_rows.length; i++)
+			count_children(root_rows[i])
+	}
+
+	// add a newly added-to-the-dataset row to the tree
+	var add_to_tree = function(row, parent_row) {
+		if (!parent_row) {
+			if (root_rows) {
+				root_rows.push(row)
+				row.level = 0
+			}
+			return
+		}
+		// set parent->child and child->parent links
+		row.parent_row = parent_row
+		row.level = parent_row.level + 1
+		row.childcount = 0
+		// add to parent (nevermind at which index)
+		parent_row.children.push(row)
+		// increment childcount in all parents
+		while (parent_row) {
+			parent_row.childcount++
+			parent_row = parent_row.parent_row
+		}
+	}
+
+	// recursively mark children of a row as deleted
+	var delete_children = function(row) {
+		var rows = row.children
+		if (!rows) return
+		for (var i = 0; i < rows.length; i++) {
+			var row = rows[i]
+			row.deleted = true
+			delete_children(row)
+		}
+	}
+
+	// remove a row from the tree, updating parents
+	// NOTE: all children must be removed too, manually!
+	var remove_from_tree = function(row) {
+		var prow = row.parent_row
+		if (!prow) return
+		// remove from parent
+		remove(prow.children, prow.children.indexOf(row))
+		// update childcount in all parents
+		do {
+			prow.childcount--
+			prow = prow.parent_row
+		} while (prow)
 	}
 
 	d.parent_id = function(vri) {
@@ -345,16 +404,20 @@ dataset = function(d_opt) {
 	}
 
 	d.collapse_all = function() {
-		for (ri in rows)
-			if (ri.expanded)
-				ri.expanded = false
+		for (var ri = 0; ri < rows.length; ri++) {
+			var row = rows[ri]
+			if (row.expanded)
+				row.expanded = false
+		}
 		init_rowmap()
 	}
 
 	d.expand_all = function() {
-		for (ri in rows)
-			if (!ri.expanded)
-				ri.expanded = true
+		for (var ri = 0; ri < rows.length; ri++) {
+			var row = rows[ri]
+			if (!row.expanded)
+				row.expanded = true
+		}
 		init_rowmap()
 	}
 
@@ -370,35 +433,92 @@ dataset = function(d_opt) {
 		return oldvals && oldvals[fi] !== row.values[fi]
 	}
 
-	d.removed_rows = function() {
-		//
-	}
-
 	d.changes = function() {
-
 		var insert = []
-
 		var update = []
-
 		var remove = []
-		for (var ri in rows)
-			if (rows[ri].deleted)
-				remove.push(rows[ri].values[id_fi])
-
-		var row_order = []
-
+		var ii = 0 // insert index
+		for (var ri = 0; ri < rows.length; ri++) {
+			var row = rows[ri]
+			if (row.isnew && !row.deleted) {
+				var rec = {}
+				var values = row.values
+				for (var fi = 0; fi < fields.length; fi++) {
+					var val = values[fi]
+					if (val != null)
+						rec[fields[fi].name] = values[fi]
+				}
+				insert.push({values: rec})
+				row.ii = ii // save it for reconciliation
+				ii++
+			} else if (row.oldvalues && !row.deleted) {
+				var rec = {}
+				var oldrec = {}
+				var values = row.values
+				var oldvalues = row.oldvalues
+				for (var fi = 0; fi < fields.length; fi++) {
+					var val = values[fi]
+					var oldval = oldvalues[fi]
+					var k = fields[fi].name
+					oldrec[k] = oldval
+					if (val !== oldval)
+						rec[k] = val
+				}
+				update.push({values: rec, oldvalues: oldrec})
+			} else if (row.deleted && !row.isnew) {
+				var id = row.values[id_fi]
+				remove.push(id)
+			}
+		}
 		return {
 			insert: insert,
 			update: update,
 			remove: remove,
-			row_order: row_order,
 		}
 	}
 
-	d.reconcile = function(results) {}
-	d.changed_values = function() {}
-	d.apply_changes = function() {}
-	d.cancel_changes = function() {}
+	d.reconcile = function(results) {
+		//
+	}
+
+	var remove_row = function(ri) {
+		remove_from_tree(rows[ri])
+		remove(rows, ri)
+	}
+
+	d.apply_changes = function() {
+		for (var ri = 0; ri < rows.length; ri++) {
+			var row = rows[ri]
+			if (row.deleted) {
+				remove_row(ri)
+				ri--
+				continue
+			}
+			if (row.oldvalues)
+				row.oldvalues = null
+			if (row.isnew)
+				row.isnew = false
+		}
+		init_rowmap()
+	}
+
+	d.cancel_changes = function() {
+		for (var ri = 0; ri < rows.length; ri++) {
+			var row = rows[ri]
+			if (row.isnew) {
+				remove_row(ri)
+				ri--
+				continue
+			}
+			if (row.deleted)
+				row.deleted = false
+			if (row.oldvalues) {
+				row.values = row.oldvalues
+				row.oldvalues = null
+			}
+		}
+		init_rowmap()
+	}
 
 	// remote I/O aspect ------------------------------------------------------
 
@@ -414,7 +534,7 @@ dataset = function(d_opt) {
 	// server-side order-by expression
 	d.sort_expr = function() {
 		var s = ''
-		for (var fi in fields) {
+		for (var fi = 0; fi < fields.length; fi++) {
 			var field = fields[fi]
 			if (field.name && field.sort)
 				s += field.name+':'+field.sort
@@ -462,7 +582,9 @@ dataset = function(d_opt) {
 
 	d.load = function(success, error) {
 		function succ(data) {
+			d.fields = data.fields
 			d.rows = data.rows
+			d.fieldmap = data.fieldmap
 			d.init()
 			if (success) success()
 		}
@@ -482,45 +604,63 @@ dataset = function(d_opt) {
 	$.extend(d, d_opt)
 	d.init()
 	d.json = json
+	d.make_url = make_url
 	return d
 }
 
 
 })()
 
-var d = dataset({
-	fields: [{name: 'id'},{name: 'name'},{name: 'parent_id'}],
-	parent_id_field_name: 'parent_id',
-	rows: [
-		{values: [1,'a',null]},
-		{values: [2,'b',null]},
-		{values: [3,'c',1]},
-		{values: [4,'d',1]},
-		{values: [5,'e',3]},
-		{values: [6,'f',3]},
-		{values: [7,'g',6]},
-	],
-})
-
-/*
-d.remove(2) // remove 3
-d.remove(3) // remove 5
-d.remove(4) // remove 7
-d.insert()  // insert after the removed 7
-*/
+// helpers
 
 function str_repeat(s, n) {
 	return new Array(n+1).join(s)
 }
 
-var s = ''
-for(ri=0;ri<d.rowcount();ri++) {
-	s += str_repeat('   ',d.row(ri).level)
-	for(fi=0;fi<d.fieldcount();fi++)
-		s += d.val(ri, fi) + ' '
-	s += '\n'
+function print_tree(d) {
+	var s = ''
+	for (var ri=0;ri<d.rowcount();ri++) {
+		s += str_repeat('   ',d.row(ri).level)
+		for (var fi=0;fi<d.fieldcount();fi++)
+			s += d.val(ri, fi) + ' '
+		s += '\n'
+	}
+	console.log('--------\n'+s+'----------')
 }
-console.log(s)
 
-//console.log(d.json(d.changes()))
+// tree
+
+var d = dataset({
+	fields: [{name: 'id'},{name: 'name'},{name: 'parent_id'}],
+	parent_id_field_name: 'parent_id',
+	rows: [
+		{values: [0,'a',null]},
+		{values: [1,'b',null]},
+		{values: [2,'c',0]},
+		{values: [3,'d',0]},
+		{values: [4,'e',2]},
+		{values: [5,'f',2]},
+		{values: [6,'g',5]},
+	],
+})
+
+print_tree(d)
+d.expand_all()
+print_tree(d)
+
+d.remove(0) // remove 0 and all its children
+d.insert()  // insert at the end
+d.setval(d.rowcount()-1, 1, 'z')
+d.setval(0, 1, 'x')
+
+print_tree(d)
+console.log(d.json(d.changes()))
+
+d.cancel_changes()
+print_tree(d)
+console.log(d.json(d.changes()))
+
+// url
+
+console.log(d.make_url('/path', ['a1', 'a2'], {b: 1, a: 2}))
 
