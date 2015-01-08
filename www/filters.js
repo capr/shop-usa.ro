4c93d5cedf24a742b9ceea1734322087eb4e616e
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2015-01-08 21:59:15 +0200

    unimportant

--- www/filters.js
+++ www/filters.js
@@ -31,8 +31,24 @@ function update_filters(filters, order, q, fq) {
 		}
 	}
 
+	var selected = [] // [v1,...]
+
+	var cats = []
+	if (g_cats && g_catid) {
+		var cat = g_cats[g_catid]
+		while (cat.parent) {
+			cats.push(cat)
+			cat = cat.parent
+		}
+		for (var i = cats.length - 1; i >= 0; i--) {
+			var cat = cats[i]
+			selected.push({catid: cat.id, vname: cat.name})
+		}
+	}
+
 	// mark selected vids in filters based on the vids map.
 	// also, make a fidmap so we can track back fids from vids.
+	// also, collect selected filters.
 	var fidmap = {} // {vid: fid}
 	for (var i = 0; i < filters.length; i++) {
 		var filter = filters[i]
@@ -41,6 +57,8 @@ function update_filters(filters, order, q, fq) {
 			var v = values[j]
 			v.selected = vids[v.vid]
 			fidmap[v.vid] = filter.fid
+			if (v.selected)
+				selected.push(v)
 		}
 	}
 
@@ -74,6 +92,11 @@ function update_filters(filters, order, q, fq) {
 
 	render('filters', filters, '#filters')
 
+	if (selected.length)
+		selected[0].first = true
+
+	render('selected_filters', selected, '#selected_filters')
+
 	$('#filters a[vid]').each(function() {
 		var a = $(this)
 		var vid = parseInt(a.attr('vid'))
@@ -86,20 +109,48 @@ function update_filters(filters, order, q, fq) {
 		var fid = $(this).attr('fid')
 		var list = $(this).find('li')
 		var input = $(this).find('input')
-		if (list.length > 40)
-			input.show()
-		else
-			input.hide()
+		input.toggle(list.length > 40)
 		input.quicksearch(list).cache()
 	})
 
+	/*
+	$('#selected_filters a[catid]').each(function() {
+		var catid = $(this).attr('catid')
+		var modified_fq = build_fq(fq, 'remove', catid)
+		setlink(this, cat_url(catid, 1, order, q, modified_fq)
+	})
+	*/
+
+	$('#selected_filters a[vid]').each(function() {
+		var vid = parseInt($(this).attr('vid'))
+		var modified_fq = build_fq(fq, 'remove', vid)
+		setlink(this, cat_url(g_catid, 1, order, q, modified_fq))
+	})
+
+	filters_make_editable()
+}
+
+function filters_make_editable() {
+
+	if (!editmode())
+		return
+
+	$('#filters div[fid]').each(function() {
+		var fid = $(this).attr('fid')
+		var edit = $(this).find('.edit')
+		edit.show()
+	})
 }
 
 load_filters = function(catid, order, q, fq) {
+
 	load_content('#filters', '/filters.json/'+catid,
 		function(filters) {
 			update_filters(filters, order, q, fq)
 		})
+
+	listen('usr.editmode', filters_make_editable)
+
 }
 
 })()
