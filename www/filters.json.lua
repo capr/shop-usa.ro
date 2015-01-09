e91348ab1e1120355fd7e8689fa4bf8297689e65
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2015-01-09 22:31:22 +0200

    unimportant

--- www/filters.json.lua
+++ www/filters.json.lua
@@ -128,7 +128,7 @@ end
 --return only the filters that contain more than one value.
 local t = {}
 for i,filter in ipairs(filters) do
-	if filter.values and #filter.values > 1 then
+	if filter.values and #filter.values > 0 then
 		table.insert(t, {
 			fid = filter.fid,
 			fname = filter.fname,
