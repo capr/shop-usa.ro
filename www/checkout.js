d5e3ec7b0f2c4c1175e6a824c6e3f22e6def7495
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2015-02-09 17:49:37 +0200

    unimportant

--- www/checkout.js
+++ www/checkout.js
@@ -214,21 +214,21 @@ action.checkout = function() {
 	login() // trigger load cart
 }
 
+(function() {
+  var _fbq = window._fbq || (window._fbq = []);
+  if (!_fbq.loaded) {
+	 var fbds = document.createElement('script');
+	 fbds.async = true;
+	 fbds.src = '//connect.facebook.net/en_US/fbds.js';
+	 var s = document.getElementsByTagName('script')[0];
+	 s.parentNode.insertBefore(fbds, s);
+	 _fbq.loaded = true;
+  }
+})();
+
 action.order_placed = function() {
 	hide_nav()
 	render('order_placed', null, '#main')
-
-	(function() {
-	  var _fbq = window._fbq || (window._fbq = []);
-	  if (!_fbq.loaded) {
-		 var fbds = document.createElement('script');
-		 fbds.async = true;
-		 fbds.src = '//connect.facebook.net/en_US/fbds.js';
-		 var s = document.getElementsByTagName('script')[0];
-		 s.parentNode.insertBefore(fbds, s);
-		 _fbq.loaded = true;
-	  }
-	})();
 
 	window._fbq = window._fbq || [];
 	window._fbq.push(['track', '6024267675664', {'value':'0.00','currency':'RON'}]);
